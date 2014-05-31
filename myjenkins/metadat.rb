74c9154ee092f607f0fb0a054b7622c99fe0ed69
oliveirf <fransol@sapo.pt>
2014-05-31 10:21:14 -0400

    jenkins

---
+++ myjenkins/metadat.rb
@@ -0,0 +1,7 @@
+name        "myjenkins"
+description 'Example cookbook for Jenkins Berkshelf based install'
+maintainer  "MyOrg"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'jenkins'
---
+++ myjenkins/recipes/default.rb
@@ -0,0 +1,2 @@
+include_recipe "jenkins::10gen_repo"
+include_recipe "mongodb::default"
